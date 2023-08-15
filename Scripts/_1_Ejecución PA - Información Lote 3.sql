85a69d0cd1360547bb7a9f20a88d08d914227f77
Rodian Jasef Saby Sanabria <rjsaby@gmail.com>
2023-08-15 17:31:06 -0500

    Actualización .sql Ejecución Analítica Construcciones\Interesados
    Lote 3

---
+++ Scripts/_1_Ejecución PA - Información Lote 3.sql
@@ -0,0 +1,13 @@
+select dblink_connect('conn1', 'host=172.19.8.3 dbname=maphurricane user=cmedicion password=Serescobm');
+
+-- Llamado a Procedimiento Almacenado
+
+-- ***
+-- ***
+
+-- Llamado a la generación de csv
+call calidad_calificacion_ue_lote_3('SI');
+call calidad_interesados_derechos_lote_3('SI');
+-- ***
+
+select dblink_disconnect('conn1');
